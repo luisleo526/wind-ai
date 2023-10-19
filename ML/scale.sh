9da3208f8638c411794a90afc812ad850c64abb1
Luis <luisleo52655@gmail.com>
2023-10-20 02:14:04 +0800

    debug

--- ML/scale.sh
+++ ML/scale.sh
@@ -1,3 +1,1 @@
-./libsvm/svm-scale -y 0 1 -s range_all wind.dat > wind.scale
-#./libsvm/svm-scale -y 0 1 -s range train.dat > train.scale
-#./libsvm/svm-scale -r range test.dat > test.scale
+./libsvm/svm-scale -y 0 1 -s range wind.dat > wind.scale
