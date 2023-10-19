8ba52a4b592c287766815296f2d0d9d78258082d
Luis <luisleo52655@gmail.com>
2023-10-19 23:57:46 +0800

    update script

--- ML/run.sh
+++ ML/run.sh
@@ -1,6 +1,6 @@
-for c in 1 10 100 1000
+for c in 100 300 1000
 do
-	for g in 0.001 0.01 0.1 1
+	for g in 0.001 0.0003 0.0001
 	do
 		echo "-------------------------------------------"
 		./thundersvm/build/bin/thundersvm-train -s 3 -c $c -g $g -v 5 wind.scale
