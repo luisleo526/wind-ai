8abb9aecb65da2f7b83ff5848c4e9df49a04f758
Luis <luisleo52655@gmail.com>
2023-10-19 23:34:36 +0800

    use thundersvm

--- ML/run.sh
+++ ML/run.sh
@@ -1,5 +1,8 @@
-for c in 1 10 100 300
+for c in 1 10 100 1000
 do
-	echo "c=$c"
-	./libsvm/svm-train -s 3 -h 0 -c $c -v 5 wind.scale 
+	for g in 0.001 0.01 0.1 1
+	do
+		echo "-------------------------------------------"
+		./thundersvm/build/bin/thundersvm-train -s 3 -c $c -g $g -v 5 wind.scale
+	done
 done
