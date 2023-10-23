170aaeb091bd0686878cbaa19e6490e1e3909247
Luis <luisleo52655@gmail.com>
2023-10-24 03:17:28 +0800

    debug

--- ML/scripts/run_svm.sh
+++ ML/scripts/run_svm.sh
@@ -3,7 +3,7 @@ do
 	for k in 0 1 2 3 4
 	do
 		echo "-------------------------------------------"
-		echo "C=$c"
+		echo "C=$c, fold=$k"
 		./libsvm/svm-train -s 3 -t 2 -c $c -h 0 -q data/fold$k/train.scale libsvm_output/libsvm.model
 		./libsvm/svm-predict data/fold$k/test.scale libsvm_output/libsvm.model "libsvm_output/${c}.${k}.pred"
 	done
